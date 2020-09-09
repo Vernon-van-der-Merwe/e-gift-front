b251b7bfc1ecc869901e5fe8dbe408aabb529b61
Vernon van der Merwe <Vernon.vd.m14@gmail.com>
2020-09-09 11:30:58 +0200

    created standard layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { MerchantViewComponent } from './admin/pages/merchant-view/merchant-view.component';
 import { StandardLayoutComponent } from './admin/layout/standard-layout/standard-layout.component';
 import { AdminTopNavComponent } from './admin/components/admin-top-nav/admin-top-nav.component';
+import { LoginComponent } from './admin/pages/login/login.component';
+import { AdminSideNavComponent } from './admin/components/admin-side-nav/admin-side-nav.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { AdminTopNavComponent } from './admin/components/admin-top-nav/admin-top
     MerchantViewComponent,
     StandardLayoutComponent,
     AdminTopNavComponent,
+    LoginComponent,
+    AdminSideNavComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
