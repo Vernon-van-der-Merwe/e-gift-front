fb2cf9c5353c92709ec0a1b4b3565ea5bd721a44
Vernon van der Merwe <Vernon.vd.m14@gmail.com>
2020-09-10 11:02:12 +0200

    removed dist folder from gitignore

--- src/app/admin/components/admin-side-nav/admin-side-nav.component.ts
+++ src/app/admin/components/admin-side-nav/admin-side-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-side-nav',
   templateUrl: './admin-side-nav.component.html',
-  styleUrls: ['./admin-side-nav.component.css']
+  styleUrls: ['./admin-side-nav.component.sass']
 })
 export class AdminSideNavComponent implements OnInit {
 
